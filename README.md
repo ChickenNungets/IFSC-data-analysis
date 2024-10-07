16e0c1ff42c985700bb5f39dc0ee1570b6c8686f
Maximiliano Vazquez <maxvazquez0810@gmail.com>
2024-10-06 20:06:49 -0700

    finishing touches

--- README.md
+++ README.md
@@ -7,4 +7,38 @@ The data for this project was collected by scraping the International Federation
 
 We can view the API calls used to retrive athlete and competition data by examining the Network tab in Chrome DevTools. You can view this in more detail in the scrape.py file. For failed requests, the script will automatically retry the API call multiple times before moving on to a higher success rate.
 
+Data was collected in September 2024 and so will not reflect changes due to competitions after that date.
+
 # Data Cleaning/Validation
+
+Once the raw data was collected, it went through a cleaning process. I utilized pandas for data wrangling to handle null values, data inconsistencies, and outliers. The cleaning process involved:
+
+- Removing outlier athletes and competitions (no competitions with one participant, no athletes over 10 meters tall, etc.)
+- Ensuring athletes can compete in a competition once.
+- Competitions are created by aggregating competition records for individual athletes. I ensured that upon aggregation competition gender, time and discipline remained constant (interestingly there are 2 cases of a male competing in a female competition)
+- Handling missing values by filling or dropping as needed.
+
+# Data Analysis and Visualization
+
+The goal of this project was to create an interactive bar chart race showing the top competition climbers and how their [Elo-MMR](https://github.com/EbTech/Elo-MMR/blob/master/paper/EloMMR.pdf) ratings evolved over time. This was achieved using D3.js to create a dynamic visualization.
+
+Key features:
+
+- Filtering by gender and discipline (boulder, lead, speed).
+- Interactive controls for selecting time ranges and the number of athletes displayed.
+- Smooth animations that dynamically update the chart as you explore the data over time.
+- Additionally, I've implemented a play/pause feature that lets users automatically scroll through the timeline, providing a hands-free experience for visualizing trends.
+
+The project is hosted using GitHub Pages for easy sharing and accessibility. You can check it out here!
+
+# Future Work
+
+This project was a fun dive into data visualization and applied data science. In the future, I would like to:
+
+- Expand the data to include speed (which in its bracket tournament format lends itself to regular elo rankings) and combined boulder and lead Elo-MMR.
+- Integrate machine learning models to predict future Elo ratings based on past performance.
+- Implement a full dashboard with more in-depth visualizations (e.g., scatter plots, line charts) for trend analysis.
+
+If you’re a fellow climbing enthusiast or just into data science, feel free to fork the repo or reach out with suggestions for improvements!
+
+
--- d3-viz/script.js
+++ d3-viz/script.js
@@ -1,14 +1,15 @@
-// Load CSV data and initialize the chart
 d3.csv("elo_ratings_over_time.csv").then(function(data) {
+
     data.forEach(d => {
-      d.date = new Date(d.date);  // Parse the date
-      d.elo_rating = +d.elo_rating;  // Convert Elo rating to number
+      d.date = new Date(d.date);  
+      d.elo_rating = +d.elo_rating;  
     });
   
-    window.fullEloData = data;  // Store the full dataset globally
+    window.fullEloData = data; 
+    window.delay = 100;  // Default delay for transitions
   
     // Initialize chart with default settings
-    updateChart(new Date("2024-01-01"), 20, "female", "lead");  // Default date, gender, discipline, top N
+    updateChart(new Date("2024-01-01"), 20, "male", "boulder", window.delay);  // Default date, gender, discipline, top N
   
     // Add event listener to the "Update" button
     d3.select("#updateChart").on("click", function() {
@@ -17,21 +18,21 @@ d3.csv("elo_ratings_over_time.csv").then(function(data) {
       const gender = d3.select("#gender").node().value;
       const discipline = d3.select("#discipline").node().value;
   
-      // Update chart based on the user's input
-      updateChart(currentDate, topN * 2, gender, discipline);
+      updateChart(currentDate, topN * 2, gender, discipline, window.delay);
     });
   
     // Initialize the play bar (slider)
     const timeSlider = d3.select("#timeSlider");
     timeSlider.on("input", function() {
+      const delay = 500;
       const sliderValue = +this.value;
       const date = getDateFromSlider(sliderValue);  // Compute date from slider value
       const topN = +d3.select("#topN").node().value;
       const gender = d3.select("#gender").node().value;
       const discipline = d3.select("#discipline").node().value;
   
-      // Update chart based on the current slider date
-      updateChart(date, topN * 2, gender, discipline);
+     
+      updateChart(date, topN * 2, gender, discipline, delay);
     });
   });
   
@@ -39,15 +40,14 @@ d3.csv("elo_ratings_over_time.csv").then(function(data) {
   function getTopAthletesUpToDate(data, currentDate, topN, gender, discipline) {
     // Filter for the gender and discipline
     let filteredData = data.filter(d => {
-      return d.date <= currentDate &&  // Only consider records up to the current date
-             (gender === "all" || d.gender === gender) &&  // Filter by gender if applicable
-             (discipline === "all" || d.discipline === discipline);  // Filter by discipline if applicable
+      return d.date <= currentDate &&  
+             (d.gender === gender) &&  
+             (d.discipline === discipline); 
     });
   
-    // Create a dictionary to store the most recent Elo rating for each athlete
+    
     const mostRecentElo = {};
     filteredData.forEach(d => {
-      // If the athlete has no Elo recorded yet, or if this record is more recent, update their Elo
       if (!mostRecentElo[d.athlete_name] || d.date > mostRecentElo[d.athlete_name].date) {
         mostRecentElo[d.athlete_name] = d;
       }
@@ -55,19 +55,25 @@ d3.csv("elo_ratings_over_time.csv").then(function(data) {
   
     // Convert the result into an array and sort by Elo rating in descending order
     const topAthletes = Object.values(mostRecentElo).sort((a, b) => b.elo_rating - a.elo_rating);
-  
-    // Return the top N athletes
+
     return topAthletes.slice(0, topN);
   }
-  
-  // Function to update the D3 chart
-  function updateChart(currentDate, topN, gender, discipline) {
-    // Get the top N athletes for the given date, gender, and discipline
+
+  function getFlagEmoji(countryCode) {
+    const codePoints = countryCode
+            .toUpperCase()
+            .split('')
+            .map(char => 127397 + char.charCodeAt());
+    return String.fromCodePoint(...codePoints);
+  }
+
+
+  function updateChart(currentDate, topN, gender, discipline, delay) {
+    
     const topAthletes = getTopAthletesUpToDate(window.fullEloData, currentDate, topN, gender, discipline);
   
-    // Dynamically adjust SVG height based on topN
-    const barHeight = 30; // Reduced height per bar for more compact display
-    const svgHeight = barHeight * topN + 50; // Ensure all bars fit within the height
+    const barHeight = 30; 
+    const svgHeight = Math.max(60, barHeight * topN); 
     const svg = d3.select("#eloChart").attr("height", svgHeight);
     const width = +svg.attr("width");
   
@@ -75,62 +81,140 @@ d3.csv("elo_ratings_over_time.csv").then(function(data) {
     const xScale = d3.scaleLinear()
                      .domain([d3.min(topAthletes, d => d.elo_rating), d3.max(topAthletes, d => d.elo_rating)])
                      .range([0, width]);
+    
+    
+    svg.select(".x-axis").remove();
+                 
+    const xAxis = d3.axisTop(xScale);
+
+    svg.append("g")
+        .attr("class", "x-axis")
+        .attr("transform", "translate(0, 25)") 
+        .call(xAxis);
+      
   
     const yScale = d3.scaleBand()
                      .domain(topAthletes.map(d => d.athlete_name))
-                     .range([0, svgHeight])
-                     .padding(0.01); // Reduce padding to make bars tighter
+                     .range([20, svgHeight])
+                     .padding(0); 
   
-    // Bind the data to the bars
+ 
     const bars = svg.selectAll(".bar").data(topAthletes, d => d.athlete_name);
   
-    // Exit old bars
     bars.exit().remove();
   
-    // Enter new bars
     const barEnter = bars.enter()
         .append("g")
         .attr("class", "bar");
 
-    // Append the rectangles (bars) and text (labels)
+    // mappings for country codes to emoji flags (thanks chat GPT. And IFSC yall better start using ISO 3166-1 alpha-3 because wtf are these codes)
+    const countryToEmoji = {
+        'GER': 'DE', 'RUS': 'RU', 'GRE': 'GR', 'SUI': 'CH', 'BEL': 'BE', 
+        'FRA': 'FR', 'LUX': 'LU', 'GBR': 'GB', 'RSA': 'ZA', 'ESP': 'ES', 
+        'ITA': 'IT', 'SLO': 'SI', 'AUT': 'AT', 'CZE': 'CZ', 'UKR': 'UA', 
+        'CAN': 'CA', 'USA': 'US', 'KOR': 'KR', 'CYP': 'CY', 'TPE': 'TW', 
+        'HUN': 'HU', 'HKG': 'HK', 'JPN': 'JP', 'BUL': 'BG', 'GEO': 'GE', 
+        'AUS': 'AU', 'IRL': 'IE', 'IND': 'IN', 'SWE': 'SE', 'ROU': 'RO', 
+        'SVK': 'SK', 'FIN': 'FI', 'POL': 'PL', 'CHN': 'CN', 'CRO': 'HR', 
+        'NOR': 'NO', 'BRA': 'BR', 'ARG': 'AR', 'VEN': 'VE', 'MKD': 'MK', 
+        'ECU': 'EC', 'INA': 'ID', 'MAS': 'MY', 'KAZ': 'KZ', 'BLR': 'BY', 
+        'IRI': 'IR', 'NED': 'NL', 'SGP': 'SG', 'MGL': 'MN', 'NZL': 'NZ', 
+        'POR': 'PT', 'PHI': 'PH', 'SRB': 'RS', 'DEN': 'DK', 'CHI': 'CL', 
+        'THA': 'TH', 'ISR': 'IL', 'TUR': 'TR', 'UZB': 'UZ', 'KGZ': 'KG', 
+        'LAT': 'LV', 'NEP': 'NP', 'GUA': 'GT', 'MEX': 'MX', 'COL': 'CO', 
+        'PAK': 'PK', 'LTU': 'LT', 'AZE': 'AZ', 'PER': 'PE', 'EST': 'EE', 
+        'MAC': 'MO', 'BOT': 'BW', 'BRU': 'BN', 'CAM': 'KH', 'SRI': 'LK', 
+        'CRC': 'CR', 'AND': 'AD', 'HON': 'HN', 'JOR': 'JO', 'IRQ': 'IQ', 
+        'LBN': 'LB', 'ISL': 'IS', 'BIH': 'BA', 'ESA': 'SV', 'BOL': 'BO', 
+        'UGA': 'UG', 'PUR': 'PR', 'MRI': 'MU', 'KUW': 'KW', 'KSA': 'SA', 
+        'GUM': 'GU', 'NGR': 'NG', 'MAR': 'MA', 'FIJ': 'FJ', 'CMR': 'CM'
+    };
+
+    const colorScale = d3.scaleOrdinal()
+        .domain(topAthletes.map(d => d.athlete_name))  // Use athlete names as the domain
+        .range(d3.schemeObservable10);
+
+    
     barEnter.append("rect")
             .attr("x", 0)
             .attr("y", d => yScale(d.athlete_name))
             .attr("width", 0)  // Start width at 0 for smooth transitions
-            .attr("height", yScale.bandwidth());  // Use yScale.bandwidth() for consistent height
-  
+            .attr("height", yScale.bandwidth())  // Use yScale.bandwidth() for consistent height
+            .attr("fill", d => colorScale(d.athlete_name));
+
     barEnter.append("text")
             .attr("x", d => xScale(d.elo_rating) - 10)
             .attr("y", d => yScale(d.athlete_name) + yScale.bandwidth() / 2)
             .attr("dy", ".35em")
             .attr("fill", "white")
             .attr("text-anchor", "end")
-            .text(d => d.athlete_name);
+            .text(d => `${d.athlete_name} ${getFlagEmoji(countryToEmoji[d.country])}`);
+//            .text(d => d.athlete_name);
   
-    // Merge enter and update selections
     const barUpdate = barEnter.merge(bars);
   
     // Update existing and newly entered bars
     barUpdate.transition()
-        .duration(800)
+        .duration(delay)
+        .ease(d3.easeLinear)
         .select("rect")
-        .attr("width", d => xScale(d.elo_rating))  // Ensure width is updated for both enter and update
-        .attr("y", d => yScale(d.athlete_name))    // Ensure y-position is updated for both enter and update
-        .attr("height", yScale.bandwidth());       // Ensure height is updated for consistency
+        .attr("width", d => xScale(d.elo_rating))  
+        .attr("y", d => yScale(d.athlete_name))    
+        .attr("fill", d => colorScale(d.athlete_name))
+        .attr("height", yScale.bandwidth());       
 
+    
     // Update text labels for both new and existing bars
     barUpdate.select("text")
         .transition()
-        .duration(800)
+        .duration(delay)
+        .delay((d, i) => i * 20)
         .attr("x", d => xScale(d.elo_rating) - 10)
         .attr("y", d => yScale(d.athlete_name) + yScale.bandwidth() / 2)
-        .text(d => d.athlete_name);
+        .text(d => `${d.athlete_name} ${getFlagEmoji(countryToEmoji[d.country])}`);
+        //.text(d => d.athlete_name);
   
     // Also handle the update for y-axis labels in case they change positions
     svg.selectAll(".bar")
        .attr("transform", d => `translate(0, ${yScale(d.athlete_name)})`);
 }
 
+    let isPlaying = false;  // Track if the slider is currently playing
+    let playInterval;  // Variable to hold the interval
+
+    // Play button logic
+    d3.select("#playButton").on("click", function() {
+    if (isPlaying) {
+        // If playing, pause the slider
+        clearInterval(playInterval);  
+        d3.select(this).text("▶️"); 
+    } else {
+        // If paused, start playing
+        d3.select(this).text("⏸️");  
+        playSlider();  
+    }
+    isPlaying = !isPlaying; 
+    });
+
+    // Function to play the slider
+    function playSlider() {
+    const timeSlider = d3.select("#timeSlider").node();
+    playInterval = setInterval(function() {
+        let sliderValue = +timeSlider.value;
+        const maxValue = +timeSlider.max;
+
+        // Increment the slider if it's not at the end
+        if (sliderValue < maxValue) {
+        timeSlider.value = sliderValue + 1;
+        timeSlider.delay = 50;
+        timeSlider.dispatchEvent(new Event('input'));  
+        } else {
+        clearInterval(playInterval);  // Stop playing when the slider reaches the end
+        d3.select("#playButton").text("▶️ Play");  
+        isPlaying = false;  
+        }
+    }, 400);  
+    }
 
   
   // Function to calculate the current date from the slider value
@@ -138,10 +222,8 @@ d3.csv("elo_ratings_over_time.csv").then(function(data) {
     const startDate = new Date(d3.select("#startDate").node().value);
     const endDate = new Date(d3.select("#endDate").node().value);
   
-    // Compute the date range between the start and end date
     const timeRange = endDate.getTime() - startDate.getTime();
   
-    // Calculate the corresponding date for the given slider value (0-100)
     const currentDate = new Date(startDate.getTime() + (sliderValue / 100) * timeRange);
   
     return currentDate;
